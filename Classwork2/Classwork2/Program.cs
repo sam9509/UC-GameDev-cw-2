0cea16ff71e322f609d513e5f004acfdb47c24d7
Seyed Ali Seyed Hossein <seyed.a.farzaneh@hotmail.com>
2022-06-25 19:09:18 +0300

    Uploaded Exercise files

---
+++ Classwork2/Classwork2/Program.cs
@@ -0,0 +1,40 @@
+﻿
+// This code contains the if staitment classwork.
+
+string heroName = "Sam";
+int heroPower = 1000;
+
+string villainName = "Madara";
+int villainPower = 950;
+
+if (heroPower == villainPower)
+
+{
+    Console.WriteLine(
+
+        heroName + " and "   + villainName + " are at equivalent power!!!"
+        );
+
+}
+
+else if (heroPower < villainPower)
+
+{
+
+    Console.WriteLine(
+
+       heroName+ " is weaker than " + villainName + " :(("
+       );
+
+}
+
+else
+
+    Console.WriteLine(
+
+            heroName + " is Stronger than " + villainName + " XD"
+           );
+
+{
+
+}
---
+++ Classwork2Func/Classwork2Func/Program.cs
@@ -0,0 +1,59 @@
+﻿//This Code contains function classwork
+
+float playerSpeed = 5f;
+
+float oldSpeed;
+
+SetSpeed(7f);
+
+CompareSpeed();
+
+
+void SetSpeed(float inSpeed)
+{
+    Console.WriteLine(
+
+        "The player speed is " + playerSpeed
+
+        );
+    oldSpeed = playerSpeed;
+    playerSpeed = inSpeed;
+
+    Console.WriteLine(
+
+        "The player speed is " + playerSpeed
+
+        );
+
+}
+
+
+
+void CompareSpeed ()
+
+{
+
+    if (playerSpeed == oldSpeed)
+
+
+
+        Console.WriteLine(
+
+        "The player speed before and after is " + playerSpeed
+            );
+
+    else if (playerSpeed > oldSpeed)
+
+        Console.WriteLine(
+
+        "The player Greatest speed is " + playerSpeed
+            );
+    else
+
+        Console.WriteLine(
+
+           "The player Greatest speed is " + oldSpeed
+               );
+
+}
+
